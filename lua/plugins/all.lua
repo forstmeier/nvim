8136ed077175f68d515ff2ff5c40ada1a7b309b1
John Forstmeier <john.forstmeier@gmail.com>
2026-03-16 11:10:34 -0400

    Fix Telescope search functionality

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -191,5 +191,21 @@ return {
         tag = "0.1.8",
         lazy = false,
         dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--fixed-strings",
+                    },
+                },
+            })
+        end,
     },
 }
